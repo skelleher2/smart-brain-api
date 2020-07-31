4a58d28a1e8c9f742b32fd6dc451185e242bba53
skelleher2 <52292132+skelleher2@users.noreply.github.com>
2020-07-31 17:34:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ const db = knex({
 const app = express();
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://facerecognitionbrain-sk.herokuapp.com/');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT,    PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
