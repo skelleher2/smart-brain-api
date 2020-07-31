d6a8aa251d2297eace33a42eb021ad43b4eb916a
skelleher2 <52292132+skelleher2@users.noreply.github.com>
2020-07-31 14:37:19 -0500

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: 'a69dfdadfa9f4550ad266613981209d8'
+ apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
