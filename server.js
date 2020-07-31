1d6006794abb0fa4e89f50e178527e6f36317233
skelleher2 <52292132+skelleher2@users.noreply.github.com>
2020-07-31 17:00:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -19,14 +19,23 @@ const db = knex({
 });
 
 const app = express();
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-const corsOptions = {
-  origin: 'https://facial-recognition-machine.herokuapp.com',
-  optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions));
-app.options('*', cors());
-app.use('/', routes);
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT,    PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use(bodyParser.json());
 
