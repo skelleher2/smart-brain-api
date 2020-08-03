a86cbaf166dd10aae9fc860b377e5f0c87d7212e
skelleher2 <52292132+skelleher2@users.noreply.github.com>
2020-08-03 18:45:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
@@ -8,6 +9,8 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -15,8 +18,6 @@ const db = knex({
     ssl: true,
   }
 });
-
-const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -28,6 +29,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on 3000');
-})
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
+});
